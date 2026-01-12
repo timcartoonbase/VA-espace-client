79fc48b9d9cc0daec9e335971ce80f24f4c97bae
timcartoonbase <tim@cartoonbase.com>
2026-01-12 19:23:07 +0000

    fix: update footer component class names and adjust CSS styles

--- va-espace-client/src/components/footer/Footer.jsx
+++ va-espace-client/src/components/footer/Footer.jsx
@@ -5,11 +5,11 @@ import logo from "../../assets/images/logo.svg";
 
 const Footer = ({}) => {
   return (
-    <section className={"footer"}>
+    <section className={"footer-block"}>
       <div className="section is-flex is-align-items-center">
         <div className="container">
           <div className={"columns is-justify-content-center"}>
-            <figure className="image">
+            <figure className="image logo">
               <img src={logo} />
             </figure>
           </div>
